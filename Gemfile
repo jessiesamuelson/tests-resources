ae7d0828412059c3dc8e51ac27953ea8ec45bb03
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2014-12-29 17:58:08 -0500

    Make underscore template work by moving templates folder & adding ejs gem

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
 # Windows does not come with time zone data
 gem "tzinfo-data", platforms: [:mswin, :mingw]
 
+gem 'ejs'
 
 gem 'sass', '~> 3.4.9'
 gem 'font-awesome-sass'
--- config.rb
+++ config.rb
@@ -23,7 +23,7 @@
 # with_layout :admin do
 #   page "/admin/*"
 # end
-page '/channel-manager'
+
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
