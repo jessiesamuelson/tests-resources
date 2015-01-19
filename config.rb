a78f326ce0ab1ba7bcc7beae1ea441809f7c5ee3
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-01-19 18:16:52 -0500

    Restore asset dirs for production

--- config.rb
+++ config.rb
@@ -47,12 +47,12 @@
 #   end
 # end
 
-# set :css_dir, 'admin/services/waywiremanager/stylesheets'
-# set :js_dir, 'admin/services/waywiremanager/javascripts'
-# set :images_dir, 'admin/services/waywiremanager/images'
+set :css_dir, 'admin/services/waywiremanager/stylesheets'
+set :js_dir, 'admin/services/waywiremanager/javascripts'
+set :images_dir, 'admin/services/waywiremanager/images'
 
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
+# set :css_dir, 'stylesheets'
+# set :js_dir, 'javascripts'
 
 # Build-specific configuration
 configure :build do
