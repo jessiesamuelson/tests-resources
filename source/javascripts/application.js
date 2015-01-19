0fc73b605970be05d98435fe80ed43f943a7b642
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-01-19 18:36:32 -0500

    Include javascript tags manually in layout

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -10,21 +10,21 @@
 //= require_tree .
 
 //$.getScript('javascripts/library/jquery-1.11.2.min.js', function() {});
-$.getScript('javascripts/library/underscore.js', function() {});
-$.getScript('javascripts/library/backbone.js', function() {});
-//$.getScript('javascripts/templates/channel_index.jst.ejs', function() {})
-$.getScript('javascripts/rails_api.js', function() {});
-$.getScript('javascripts/models/channel.js', function() {});
-$.getScript('javascripts/models/category.js', function() {});
-$.getScript('javascripts/collections/channels.js', function() {});
-$.getScript('javascripts/views/channels/channels_index.js', function() {});
-$.getScript('javascripts/routers/channels_router.js', function() {});
-$.getScript('javascripts/curatorVideo.js', function() {});
-$.getScript('javascripts/helpers.js', function() {})
-$.getScript('javascripts/navigation.js', function() {});
-$.getScript('javascripts/paginate.js', function() {});
+// $.getScript('javascripts/library/underscore.js', function() {});
+// $.getScript('javascripts/library/backbone.js', function() {});
+// //$.getScript('javascripts/templates/channel_index.jst.ejs', function() {})
+// $.getScript('javascripts/rails_api.js', function() {});
+// $.getScript('javascripts/models/channel.js', function() {});
+// $.getScript('javascripts/models/category.js', function() {});
+// $.getScript('javascripts/collections/channels.js', function() {});
+// $.getScript('javascripts/views/channels/channels_index.js', function() {});
+// $.getScript('javascripts/routers/channels_router.js', function() {});
+// $.getScript('javascripts/curatorVideo.js', function() {});
+// $.getScript('javascripts/helpers.js', function() {})
+// $.getScript('javascripts/navigation.js', function() {});
+// $.getScript('javascripts/paginate.js', function() {});
 
-setTimeout(function() {
+$(function() {
   RailsApi.initialize();
 
   authenticate();
@@ -36,4 +36,4 @@ setTimeout(function() {
     toggleSection($(e.target));
   });
 
-}, 300);
+});
