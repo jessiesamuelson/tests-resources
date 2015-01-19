7f455696883555433d7db95349a849cb7baa9135
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-01-19 18:15:10 -0500

    Require js files manually

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -8,3 +8,18 @@
 //= require_tree ./views
 //= require_tree ./routers
 //= require_tree .
+
+$.getScript('javascripts/library/jquery-1.11.2.min.js', function() {})
+$.getScript('javascripts/library/underscore.js', function() {})
+$.getScript('javascripts/library/backbone.js', function() {})
+//$.getScript('javascripts/templates/channel_index.jst.ejs', function() {})
+$.getScript('javascripts/rails_api.js', function() {})
+$.getScript('javascripts/models/channel.js', function() {})
+$.getScript('javascripts/models/category.js', function() {})
+$.getScript('javascripts/collections/channels.js', function() {})
+$.getScript('javascripts/views/channels/channels_index.js', function() {})
+$.getScript('javascripts/routers/channels_router.js', function() {})
+$.getScript('javascripts/curatorVideo.js', function() {})
+$.getScript('javascripts/helpers.js', function() {})
+$.getScript('javascripts/navigation.js', function() {})
+$.getScript('javascripts/paginate.js', function() {})
