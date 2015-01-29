b22602ad72583e19433a6fbf3cab64a9818e4afe
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-01-29 10:53:38 -0500

    Dynamically assign baseUrl and apiKey

--- source/javascripts/rails_api.js
+++ source/javascripts/rails_api.js
@@ -9,6 +9,19 @@ window.RailsApi = {
   }
 };
 
-window.baseUrl = 'http://staging.waywire.com:3001/api';
- // Production 'http://waywire.com/admin/services/waywiremanager';
-window.apiKey = 'da388efde809cb227c3d43ab2f170e5a';
+window.baseUrl = window.location.hostname+':3001/api';
+window.apiKey;
+
+var keys = {
+  staging: 'da388efde809cb227c3d43ab2f170e5a',
+  dev: 'a67c31bc8ce25b2906f20108dee41766',
+  production: 'ea2c98f3ef6344164fc1ba7d0b7b89cc'
+}
+
+if (baseUrl.indexOf('staging') > -1) {
+  window.apiKey = keys.staging;
+} else if (baseUrl.indexOf('nessa.dev') > -1) {
+  window.apiKey = keys.dev;
+} else {
+  window.apiKey = keys.production;
+}
