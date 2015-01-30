13f93eb3a2be0c48f10a40d5436b5da5ad93f253
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-01-30 15:53:45 -0500

    Add localhost to dev environments

--- source/javascripts/rails_api.js
+++ source/javascripts/rails_api.js
@@ -16,12 +16,13 @@ var keys = {
   staging: 'da388efde809cb227c3d43ab2f170e5a',
   dev: 'a67c31bc8ce25b2906f20108dee41766',
   production: 'ea2c98f3ef6344164fc1ba7d0b7b89cc'
-}
+};
 
 if (baseUrl.indexOf('staging') > -1) {
   window.apiKey = keys.staging;
-} else if (baseUrl.indexOf('nessa.dev') > -1) {
-  window.apiKey = keys.dev;
+} else if (baseUrl.indexOf('nessa.dev') > -1 || baseUrl.indexOf('localhost') > -1) {
+  window.apiKey = keys.staging;
+  window.baseUrl = 'http://staging.waywire.com:3001/api';
 } else {
   window.apiKey = keys.production;
 }
