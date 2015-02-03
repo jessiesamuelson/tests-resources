0509acb692f4213a5010f654d10e99a558464f06
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-02-03 17:31:49 -0500

    Restore dev base url

--- source/javascripts/rails_api.js
+++ source/javascripts/rails_api.js
@@ -21,8 +21,7 @@ var keys = {
 if (baseUrl.indexOf('staging') > -1) {
   window.apiKey = keys.staging;
 } else if (baseUrl.indexOf('nessa.dev') > -1 || baseUrl.indexOf('localhost') > -1) {
-  window.apiKey = keys.staging;
-  window.baseUrl = 'http://staging.waywire.com:3001/api';
+  window.apiKey = keys.dev;
 } else {
   window.apiKey = keys.production;
 }
