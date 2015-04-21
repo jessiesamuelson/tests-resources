09a56eb16f31e9d373c8b0dfaaa4bf284f64a814
Dev Team <devteam@waywire.com>
2015-04-21 11:27:22 -0700

    Update gem & minor tweak to channel update

--- source/javascripts/views/channels/channels_index.js
+++ source/javascripts/views/channels/channels_index.js
@@ -1,6 +1,6 @@
 RailsApi.Views.ChannelView = Backbone.View.extend({
   tagName: 'li',
-  template: _.template("<h4 class=\'name\'> <span class=\'value\'> <%= name %> <\/span> (Id: <%= id %>)<\/h4> <p class=\'href\'> <label>Link:<\/label> <span class=\'value\'> <%= href %> <\/span> <\/p> <p class='src'> <label>Chicklet:<\/label> <span class=\'value\'> <%= src %> <\/span> <\/p> <p class='image_large'> <label>Image Large:<\/label> <span class=\'value\'> <%= image_large %> <\/span> <\/p> <p class=\'category_array\'> <label>Categories:<\/label> <span class=\'value\'> <%= category_array %> <\/span> <\/p> <p class=\'sort_order\'> <label>Sort order:<\/label> <span class=\'value\'> <%= sort_order %> <\/span> <\/p> <div class=\'more\'> <span class=\'details\'>Details<\/span> <span class=\'curator\'>Curator info<\/span> <\/div> <div class=\'details\'> <p class=\'desc\'> <label>Description:<\/label> <span class=\'value\'> <%= desc %> <\/span> <\/p> <p class=\'keywords\'> <label> Keywords:<\/label> <span class=\'value\'> <%= keywords %> <\/p> <p class=\'mobile_banner\'> <label>Mobile banner:</label> <span class=\'value\'> <%= mobile_banner %> <\/span> <\/p> <p class=\'banner_img\'> <label>Channel banner:<\/label> <span class=\'value\'> <%= banner_img %> <\/span> <\/p> <p class=\'twitter_widget_id\'> <label>Twitter widget id:<\/label> <span class=\'value\'> <%= twitter_widget_id %> <\/span> <\/p> <\/div> <div class=\'curator\'> <p class=\'curator_img\'> <label>Curator image:<\/label> <span class=\'value\'> <%= curator_img %> <\/span> <\/p> <p class=\'curator_name\'> <label>Curator name:<\/label> <span class=\'value\'> <%= curator_name %> <\/span> <\/p> <p class=\'curator_desc\'> <label>Curator description:<\/label> <span class=\'value\'> <%= curator_desc %> <\/span> <\/p> <p class=\'curator_video\'> <label>Curator video:<\/label> <span class=\'value\'> <%= curator_video %> <\/span> <\/p> <\/div> <button class=\'edit-channel\' data=\'<%- id %>\'>Edit<\/button> <button class=\'delete-channel\' data=\'<%- id %>\'>Delete<\/button>"), 
+  template: _.template("<h4 class=\'name\'> <span class=\'value\'> <%= name %> <\/span> (Id: <%= id %>)<\/h4> <p class=\'href\'> <label>Link:<\/label> <span class=\'value\'> <%= href %> <\/span> <\/p> <p class='src'> <label>Chicklet:<\/label> <span class=\'value\'> <%= src %> <\/span> <\/p> <p class='image_large'> <label>Image large:<\/label> <span class=\'value\'> <%= image_large %> <\/span> <\/p> <p class=\'category_array\'> <label>Categories:<\/label> <span class=\'value\'> <%= category_array %> <\/span> <\/p> <p class=\'sort_order\'> <label>Sort order:<\/label> <span class=\'value\'> <%= sort_order %> <\/span> <\/p> <div class=\'more\'> <span class=\'details\'>Details<\/span> <span class=\'curator\'>Curator info<\/span> <\/div> <div class=\'details\'> <p class=\'desc\'> <label>Description:<\/label> <span class=\'value\'> <%= desc %> <\/span> <\/p> <p class=\'keywords\'> <label> Keywords:<\/label> <span class=\'value\'> <%= keywords %> <\/p> <p class=\'mobile_banner\'> <label>Mobile banner:</label> <span class=\'value\'> <%= mobile_banner %> <\/span> <\/p> <p class=\'banner_img\'> <label>Channel banner:<\/label> <span class=\'value\'> <%= banner_img %> <\/span> <\/p> <p class=\'twitter_widget_id\'> <label>Twitter widget id:<\/label> <span class=\'value\'> <%= twitter_widget_id %> <\/span> <\/p> <\/div> <div class=\'curator\'> <p class=\'curator_img\'> <label>Curator image:<\/label> <span class=\'value\'> <%= curator_img %> <\/span> <\/p> <p class=\'curator_name\'> <label>Curator name:<\/label> <span class=\'value\'> <%= curator_name %> <\/span> <\/p> <p class=\'curator_desc\'> <label>Curator description:<\/label> <span class=\'value\'> <%= curator_desc %> <\/span> <\/p> <p class=\'curator_video\'> <label>Curator video:<\/label> <span class=\'value\'> <%= curator_video %> <\/span> <\/p> <\/div> <button class=\'edit-channel\' data=\'<%- id %>\'>Edit<\/button> <button class=\'delete-channel\' data=\'<%- id %>\'>Delete<\/button>"), 
 
   initialize: function() {
     this.listenTo(this.model, 'change', this.render);
@@ -89,10 +89,9 @@ RailsApi.Views.ChannelView = Backbone.View.extend({
 
   updateChannel: function(e) {
     e.preventDefault();
-    
     var name = $(this.el).find('.name').val(),
         src = $(this.el).find('.src').val(),
-        image_large = $(this.el).find('.image_large'),
+        image_large = $(this.el).find('.image_large').val(),
         href = $(this.el).find('.href').val(),
         category_array = $(this.el).find('.category_array').val().toLowerCase().split(','),
         sort_order = parseInt($(this.el).find('.sort_order').val()),
@@ -116,7 +115,7 @@ RailsApi.Views.ChannelView = Backbone.View.extend({
       curator_name: curator_name, curator_img: curator_img,
       curator_desc: curator_desc, curator_video: curator_video
     });
-
+    
     this.model.url = baseUrl+'/channels/'+id;
     this.model.save(null, {
       success: function(data) {
