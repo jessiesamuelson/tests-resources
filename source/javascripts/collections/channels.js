42a29447b79905f0d4b90b49605e66dd45df6126
Jessie Samuelson <jessie.samuelson@staff.waywire.com>
2015-12-23 12:45:53 -0500

    removes unnecessary files

--- source/javascripts/collections/channels.js
+++
@@ -1,7 +0,0 @@
-RailsApi.Collections.Channels = Backbone.Collection.extend({
-  model: RailsApi.Models.Channel,
-  url: '/channels',
-  parse: function(response) {
-    return response.channels;
-  }
-});
